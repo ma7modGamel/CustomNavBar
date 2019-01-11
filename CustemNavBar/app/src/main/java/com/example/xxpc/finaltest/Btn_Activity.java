54457101e1c24383c3fd9dedb7102bb93dddb84c
ma7modGamel <mahmoudgamel10@gmail.com>
2019-01-11 22:15:35 +0200

    nvBarSchoola

---
+++ CustemNavBar/app/src/main/java/com/example/xxpc/finaltest/Btn_Activity.java
@@ -0,0 +1,13 @@
+package com.example.xxpc.finaltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Btn_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_btn_);
+    }
+}
---
+++ CustemNavBar/app/src/main/java/com/example/xxpc/finaltest/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.xxpc.finaltest;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout= findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
